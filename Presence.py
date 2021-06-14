e8e1a32787cc6611d96645a72d988c16d86f2157
“AlekSimpson” <alek@asu.me>
2021-06-14 09:18:44 -0700

    updated server code with MQTT

--- Presence.py
+++ Presence.py
@@ -1,31 +1,21 @@
-from flask import Flask, request
 from KitchenLightsManager import kitchenLightOn, kitchenLightOff
-app = Flask(__name__)
-@app.route('/', methods=['POST'])
-
-def receieveData():
-    return (request.form('room'), request.form('powered')) # On the sending end you will send a json payload and foo (for example) would be the key to whatever data it is representing
 
 #Room - entered,
 
 # Room codes: 0 - Kitchen
 # Room functions: 0 - light off, 1 - light on
 
-def loop():
+def lightMan(room, powered):
     kitchenFunctions = [kitchenLightOff, kitchenLightOn]
     roomFunctions = [kitchenFunctions]
 
-    lightData = receieveData()
-    room = lightData[0]
-    powered = lightData[1]
-
     roomFunctions[room][powered]()
 
-def main(looping):
-    if looping:
-        while True:
-            loop()
-    else:
-        loop()
-
-main()
+# def main(looping):
+#     if looping:
+#         while True:
+#             loop()
+#     else:
+#         loop()
+#
+# main()
---
+++ mqtt_sub.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+from Presence import lightMan
+import json
+
+MQTT_SERVER = "localhost"
+MQTT_PATH = "mqtt/presence"
+
+def on_connect(client, userdata, flags, rc):
+    print("connected with result code " + str(rc))
+    client.subscribe(MQTT_PATH)
+
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+    payload = json.loads(msg.payload)
+
+    powered = ""
+    room = ""
+
+    for key, value in payload.items():
+        powered = int(value)
+        room = int(key)
+
+    lightMan(room, powered)
+
+cliet = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_SERVER, 1883, 60)
+
+client.loop_forever()
